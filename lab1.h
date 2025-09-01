3dd80a6092e001b4a1991ce9ff844e12013cd46d
Mawlle <mawwlle@yandex.ru>
2025-09-01 17:59:20 +0300

    feat: lab content

---
+++ lab1.h
@@ -0,0 +1,31 @@
+#ifndef LAB1_H
+#define LAB1_H
+
+// Задача 1
+void min_max(int a, int b, int *min, int *max);
+
+// Задача 2
+int is_even(int n); // 1 - четное, 0 - нечетное
+
+// Задача 3
+int sign(int n); // -1, 0, 1
+
+// Задача 4
+int absolute(int n);
+
+// Задача 5
+int max_of_three(int a, int b, int c);
+
+// Задача 6
+const char* grade(int score); // "отлично", "хорошо", ...
+
+// Задача 7
+double distance(double x1, double y1, double x2, double y2);
+
+// Задача 8
+int is_prime(int n); // проверка простоты (1 - простое, 0 - составное)
+
+// Задача 9
+int count_multiples(int start, int end, int k);
+
+#endif
---
+++ solutions.c
@@ -0,0 +1,58 @@
+#include "lab1.h"
+#include <math.h>
+#include <stdlib.h>
+
+// Задача 1
+void min_max(int a, int b, int *min, int *max) {
+    // TODO: реализовать
+    *min = 0;
+    *max = 0;
+}
+
+// Задача 2
+int is_even(int n) {
+    // TODO: реализовать
+    return 0;
+}
+
+// Задача 3
+int sign(int n) {
+    // TODO: реализовать
+    return 0;
+}
+
+// Задача 4
+int absolute(int n) {
+    // TODO: реализовать
+    return 0;
+}
+
+// Задача 5
+int max_of_three(int a, int b, int c) {
+    // TODO: реализовать
+    return 0;
+}
+
+// Задача 6
+const char* grade(int score) {
+    // TODO: реализовать
+    return "не реализовано";
+}
+
+// Задача 7
+double distance(double x1, double y1, double x2, double y2) {
+    // TODO: реализовать
+    return 0.0;
+}
+
+// Задача 8
+int is_prime(int n) {
+    // TODO: реализовать
+    return 0;
+}
+
+// Задача 9
+int count_multiples(int start, int end, int k) {
+    // TODO: реализовать
+    return 0;
+}
